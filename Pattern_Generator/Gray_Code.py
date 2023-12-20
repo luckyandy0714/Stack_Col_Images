b7511c679186d39491c74a61f3e266eff4cca48e
luckyandy0714 <97470341+luckyandy0714@users.noreply.github.com>
2023-12-20 22:11:51 +0800

    Update Gray_Code.py

--- Pattern_Generator/Gray_Code.py
+++ Pattern_Generator/Gray_Code.py
@@ -6,20 +6,20 @@ height = 720
 pattern_num = 8
 
 for i in range(0, pattern_num):
-    num_stripes = 2 ** (i + 1)
-    stripe_width = int(width / num_stripes)
     pattern = np.zeros((height, width), dtype=np.uint8)
+
+    num_stripes = 2 ** (i + 1)
     bit_negate = False
     for j in range(num_stripes):
-        start_col = int(j * stripe_width)
-        end_col = int(start_col + stripe_width)
+        start_col = int((j / num_stripes) * width)
+        end_col = int(((j + 1) / num_stripes) * width)
         if not bit_negate:
-            if (j + 2) % 2 != 0:
-                pattern[:, start_col:end_col] = 255                
-        else:
-            if (j + 2) % 2 != 1:
+            if j % 2 != 0:
                 pattern[:, start_col:end_col] = 255
-        if (j + 2) % 2 == 1:
+        else:
+            if j % 2 != 1:
+                pattern[:, start_col:end_col] = 255
+        if j % 2 == 1:
             bit_negate = not bit_negate
 
     #cv2.imshow(f"Gray_Code_{i}", pattern)
