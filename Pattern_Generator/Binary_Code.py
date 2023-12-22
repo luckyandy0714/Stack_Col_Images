bda13441f73a260377b85347c399a943c5f007d3
luckyandy0714 <97470341+luckyandy0714@users.noreply.github.com>
2023-12-22 15:16:20 +0800

    Update Binary_Code.py

--- Pattern_Generator/Binary_Code.py
+++ Pattern_Generator/Binary_Code.py
@@ -7,15 +7,14 @@ pattern_num = 8
 
 for i in range(0, pattern_num):
     num_stripes = 2 ** (i + 1)
-    stripe_width = int(width / num_stripes)
     pattern = np.zeros((height, width), dtype=np.uint8)
     for j in range(num_stripes):
-        start_col = int(j * stripe_width)
-        end_col = int(start_col + stripe_width)
+        start_col = int((j / num_stripes) * width)
+        end_col = int(((j + 1) / num_stripes) * width)
         if (j + 2) % 2 != 0:
             pattern[:, start_col:end_col] = 255
-
-    #cv2.imshow(f"Binary_Code_{i}", pattern)
+    #pattern = cv2.rotate(pattern, cv2.ROTATE_90_CLOCKWISE)
+    #cv2.imshow(f"Binary_Code_{i}", pattern)   
     cv2.imwrite(f"Binary_Code_{i}.jpg", pattern)
 
 cv2.waitKey(0)
